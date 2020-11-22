089248cc8d1cda78364ce8d25c2c7686f890214c
marcobelo <marco.barone.belo@gmail.com>
2020-11-22 14:08:46 -0300

    insert into aluno, exploring common data types

--- course_1/sql/1_table_aluno.sql
+++ course_1/sql/1_table_aluno.sql
@@ -1,5 +1,5 @@
 -- Criando tabela aluno, explorando principais tipos de fields:
-CREATE TABLE aluno (
+CREATE TABLE IF NOT EXISTS aluno (
     id SERIAL,
     nome VARCHAR(255),
     cpf CHAR(11),
@@ -12,3 +12,28 @@ CREATE TABLE aluno (
     hora_aula TIME,
     matriculado_em TIMESTAMP
 );
+-- Inserindo um novo aluno
+INSERT INTO aluno (
+        nome,
+        cpf,
+        observacao,
+        idade,
+        dinheiro,
+        altura,
+        ativo,
+        data_nascimento,
+        hora_aula,
+        matriculado_em
+    )
+VALUES (
+        'Marco',
+        '12345678901',
+        'Lorem Ipsum is simply dummy text of the printing...',
+        30,
+        18023.50,
+        1.72,
+        TRUE,
+        '1990-06-04',
+        '08:30:00',
+        '2020-07-18 14:35:02'
+    );
