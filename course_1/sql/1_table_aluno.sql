8cba37e9bb3683207e66a748bfad7601195c1b1f
marcobelo <marco.barone.belo@gmail.com>
2020-11-23 10:48:24 -0300

    Updating and deleting an item from aluno table

--- course_1/sql/1_table_aluno.sql
+++ course_1/sql/1_table_aluno.sql
@@ -37,3 +37,19 @@ VALUES (
         '08:30:00',
         '2020-07-18 14:35:02'
     );
+-- Atualizando dados do aluno criado
+UPDATE aluno
+SET nome = 'Mulder',
+    cpf = '12312332112',
+    observacao = 'nova obs',
+    idade = 51,
+    dinheiro = 5322345.23,
+    altura = 1.70,
+    ativo = FALSE,
+    data_nascimento = '1980-12-12',
+    hora_aula = '18:30:00',
+    matriculado_em = '2010-07-18 10:35:02'
+WHERE id = 1;
+-- Removendo o aluno
+DELETE FROM aluno
+WHERE nome = 'Mulder';
---
+++ makefile
@@ -0,0 +1,2 @@
+run_course_1_db:
+	docker-compose -f course_1/docker-compose.yml up
