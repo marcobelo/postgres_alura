1d11185eb857b8644264adf84c09135428d8f7e8
marcobelo <marco.barone.belo@gmail.com>
2020-11-28 10:52:21 -0300

    creating views

---
+++ course_2/sql/4_view.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE VIEW vw_cursos_por_categoria AS
+SELECT categoria.nome AS categoria,
+    COUNT(curso.id) AS numero_cursos
+FROM categoria
+    JOIN curso ON curso.categoria_id = categoria.id
+GROUP BY categoria;
+------------
+CREATE OR REPLACE VIEW vw_cursos_programacao AS
+SELECT nome
+FROM curso
+WHERE categoria_id = 2;
